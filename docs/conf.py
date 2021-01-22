196370138db9503228f8947d17fa251f0270ab74
Nicola Bastianello <nicola.bastianello@protonmail.com>
2021-01-22 23:23:19 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = ['sphinx.ext.autodoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
