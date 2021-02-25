e56b9e9a115e410acf3f25f713098078c2df44b0
Nicola Bastianello <nicola.bastianello@protonmail.com>
2021-02-25 15:34:40 +0100

    distributed solvers

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Nicola Bastianello'
 author = 'Nicola Bastianello'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.1.5'
 
 
 # -- General configuration ---------------------------------------------------
