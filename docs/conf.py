77281c28929d9860d57988f6e682de803ce6c787
Nicola Bastianello <nicola.bastianello@protonmail.com>
2021-03-23 14:42:09 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-
+#
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
@@ -31,13 +31,16 @@ release = '0.1.8'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.autosummary',
+              'sphinx.ext.coverage',
               'sphinx.ext.napoleon',
-              'sphinx.ext.mathjax',
-]
+              'sphinx.ext.mathjax']
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = []
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -50,9 +53,10 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path = ['_static']
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. tvopt documentation master file, created by
-   sphinx-quickstart on Fri Jan 22 22:38:26 2021.
+   sphinx-quickstart on Thu Nov 12 11:55:32 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
@@ -7,22 +7,10 @@ Welcome to tvopt's documentation!
 =================================
 
 .. toctree::
-   :maxdepth: 2
+   :maxdepth: 5
    :caption: Contents:
 
-Modules
-=======
-
-.. autosummary::
-   :toctree: modules
-
-   tvopt.costs
-   tvopt.distributed_solvers
-   tvopt.networks
-   tvopt.prediction
-   tvopt.sets
-   tvopt.solvers
-   tvopt.utils
+   tvopt
 
 
 
